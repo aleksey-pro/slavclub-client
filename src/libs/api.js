62993f76ec0379089155a131f839767b744bcb61
davegahn <anxieter@yandex.ru>
2020-03-27 17:23:11 +0300

    Fix api.js

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 import { getToken } from './token';
 
+const API_URL = {
+  production: 'http://www.xn--80aaf8admgsd3i.xn--p1acf/api/v1',
+  development: 'http://localhost:3050/api/v1',
+  staging: 'http://www.xn--80aaf8admgsd3i.xn--p1acf/api/v1',
+};
+
+const TARGET = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+
 export const api = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: process.env.API,
 });
 
 export const get = (url, params = {}) => api.get(url, {
